35a45a18671f47643d13a5334954398d33fe4a6c
Guy <guyokay@gmail.com>
2023-03-11 09:48:46 -0800

    Added pytorch built in pooling and custom pooling

--- cuda_optimizations_with pytorch.py
+++ cuda_optimizations_with pytorch.py
@@ -1,35 +1,78 @@
 import torch
+import numpy as np
 
-# create a matrix with random values
-matrix = torch.randn(1024, 1024, dtype=torch.float32).cuda(non_blocking=True)
+# Needs both CUDA and CuDNN
+
+MATRIX_SIZE = 1024
+KERNEL_SIZE = 32
+
+import torch
+import torch.nn.functional as F
+
+def pytorch_builtin_pooling(input_tensor, pool_size):
+    """
+    Performs max pooling on a 2D input tensor using a square pooling window with equal stride and kernel size.
+    This function uses CUDA optimizations for improved performance, including pinned memory and the PyTorch
+    `torch.nn.functional.max_pool2d` function.
+
+    Args:
+        - input_tensor: a 2D PyTorch tensor
+        - pool_size: an integer specifying the size of the square pooling window
+
+    Returns:
+        - a 2D PyTorch tensor of pooled values
+    """
+    # Check that the pool size is valid
+    if pool_size < 1:
+        raise ValueError("Pool size must be at least 1.")
+
+    # Compute the output dimensions
+    input_height, input_width = input_tensor.size()
+    output_height = (input_height - pool_size) // pool_size + 1
+    output_width = (input_width - pool_size) // pool_size + 1
+
+    # Perform max pooling using the torch.nn.functional.max_pool2d function
+    output = F.max_pool2d(input_tensor.view(1, 1, input_height, input_width), pool_size).view(output_height,
+                                                                                              output_width)
+    return output
+
+def pytorch_custom_pooling(input_tensor, pool_size):
+    """
+    Performs max pooling on a 2D input tensor using a square pooling window with equal stride and kernel size.
+    :param input_tensor:
+    :param pool_size:
+    :return:
+    """
+    # Check that the pool size is valid
+    if pool_size < 1:
+        raise ValueError("Pool size must be at least 1.")
+
+    # Compute the output dimensions
+    input_height, input_width = input_tensor.size()
+    output_height = (input_height - pool_size) // pool_size + 1
+    output_width = (input_width - pool_size) // pool_size + 1
+
+    # Create a tensor to hold the output
+    input_pinned = torch.empty((input_height, input_width), dtype=torch.double, pin_memory=True)
+    output_pinned = torch.empty((output_height, output_width), dtype=torch.double, pin_memory=True)
+
+    # Copy input tensor to pinned memory on the CPU
+    input_pinned.copy_(input_tensor)
+
+    # Transfer pinned memory tensors to the GPU
+    input_gpu = input_pinned.to(device='cuda', non_blocking=True)
+    output_gpu = output_pinned.to(device='cuda', non_blocking=True)
+
+    # Perform max pooling on the GPU
+    output_gpu = torch.nn.functional.max_pool2d(input_gpu.view(1, 1, input_height, input_width), pool_size).view(
+        output_height, output_width)
+
+    # Transfer output tensor back to pinned memory on the CPU
+    output_pinned.copy_(output_gpu.cpu())
+
+    return output_pinned
 
 
-# torch.empty_like creates new tensor of same size and data type like input tensor.
-#pin_memory=True This flag is used to allcate output in pinned memory, which can help optimize data transfer between GPU and CPU
-#pin_memory=True this flag is used to specify GPU to CPU transfer should be non blocking.
-# allocate pinned memory for the output
-output = torch.empty_like(matrix, pin_memory=True).cuda(non_blocking=True)
-
-# loop over the rows of the matrix.
-#matrix.shape[0] = row and matrix.shape[1]=col
-for i in range(matrix.shape[0]):
-    # copy the row to pinned memory
-    row = matrix[i].pin_memory()
-
-# In case want to loop over the columns of the heat map
-#     for j in range(matrix.shape[1]):
-#         # copy the column to pinned memory
-#         col = matrix[:, j].pin_memory()
-
-    # perform operation on the row
-    row_mean = torch.mean(row)
-
-    # copy the result back to regular memory
-    output[i] = row_mean
-
-    # unpin the row
-    row.unpin_memory()
-
-# free pinned memory.Not strictly necessary, but recommended
-output.pin_memory()
-del output
+# Create a 2D tensor of random values
+input_tensor = torch.rand(MATRIX_SIZE, MATRIX_SIZE)
+pool = pytorch_custom_pooling(input_tensor, KERNEL_SIZE)
