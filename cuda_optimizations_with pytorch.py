5ca2a3220943a7796ae383628d5d54b390cd70ca
Guy <guyokay@gmail.com>
2023-03-12 16:17:03 -0700

    Added PyTorch Implementation

--- cuda_optimizations_with pytorch.py
+++ cuda_optimizations_with pytorch.py
@@ -1,16 +1,21 @@
-import torch
-import numpy as np
+
 
 # Needs both CUDA
 # Needs to run CUDA on version 11.7 and torch 1.13.1
 # pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu117
 
-MATRIX_SIZE = 2048
-KERNEL_SIZE = 16
+# nv-nsight-cu-cli --target-processes all --export output.ncu-rep python "cuda_optimizations_with pytorch.py"
+
+MATRIX_SIZE = 4096
+KERNEL_SIZE = 64
 
 import torch
+import time
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+
+DEBUG = True
+
 
 def pytorch_builtin_pooling(input_tensor, pool_size):
     """
@@ -34,10 +39,32 @@ def pytorch_builtin_pooling(input_tensor, pool_size):
     output_height = (input_height - pool_size) // pool_size + 1
     output_width = (input_width - pool_size) // pool_size + 1
 
-    # Perform max pooling using the torch.nn.functional.max_pool2d function
-    output = F.max_pool2d(input_tensor.view(1, 1, input_height, input_width), pool_size).view(output_height,
-                                                                                              output_width)
-    return output
+    # start main timer
+    total_time = time.perf_counter()
+    # Create a tensor to hold the output and move it to the GPU
+    output_gpu = torch.empty((output_height, output_width), dtype=torch.double, device='cuda')
+
+    # Perform max pooling on the GPU
+    input_gpu = input_tensor.to(device='cuda')
+
+    calc_time = time.perf_counter()
+    output_gpu = torch.nn.functional.max_pool2d(input_gpu.view(1, 1, input_height, input_width), pool_size).view(
+        output_height, output_width)
+    calc_time = time.perf_counter() - calc_time
+
+    # copy output to CPU
+    output_cpu = output_gpu.to(device='cpu')
+    # end main timer
+    total_time = time.perf_counter() - total_time
+
+    if DEBUG:
+        # print time in microseconds
+        print("Total time: " + str(total_time * 1000000) + " microseconds")
+        print("Calc time: " + str(calc_time * 1000000) + " microseconds")
+        print("Copy time: " + str((total_time - calc_time) * 1000000) + " microseconds")
+
+    return output_cpu
+
 
 def pytorch_custom_pooling(input_tensor, pool_size):
     """
@@ -56,47 +83,57 @@ def pytorch_custom_pooling(input_tensor, pool_size):
     output_width = (input_width - pool_size) // pool_size + 1
 
     # Create a tensor to hold the output
-    input_pinned = torch.empty((input_height, input_width), dtype=torch.double, pin_memory=True)
-    output_pinned = torch.empty((output_height, output_width), dtype=torch.double, pin_memory=True)
+    input_pinned = torch.empty((input_height, input_width), dtype=torch.float32, pin_memory=True)
+    output_pinned = torch.empty((output_height, output_width), dtype=torch.float32, pin_memory=True)
+
+    # start main timer
+    total_time = time.perf_counter()
 
     # Copy input tensor to pinned memory on the CPU
     input_pinned.copy_(input_tensor)
 
     # Transfer pinned memory tensors to the GPU
     input_gpu = input_pinned.to(device='cuda', non_blocking=True)
-    output_gpu = output_pinned.to(device='cuda', non_blocking=True)
 
     # Perform max pooling on the GPU
+    calc_time = time.perf_counter()
     output_gpu = torch.nn.functional.max_pool2d(input_gpu.view(1, 1, input_height, input_width), pool_size).view(
         output_height, output_width)
+    calc_time = time.perf_counter() - calc_time
 
     # Transfer output tensor back to pinned memory on the CPU
     output_pinned.copy_(output_gpu.cpu())
 
+    # end main timer
+    total_time = time.perf_counter() - total_time
+
+    if DEBUG:
+        # print time in microseconds
+        print("Total time: " + str(total_time * 1000000) + " microseconds")
+        print("Calc time: " + str(calc_time * 1000000) + " microseconds")
+        print("Copy time: " + str((total_time - calc_time) * 1000000) + " microseconds")
+
     return output_pinned
 
 
-for i in range(12):
+for i in range(1):
     """    # Create a 2D tensor of random values
     input_tensor = torch.rand(MATRIX_SIZE, MATRIX_SIZE, dtype=torch.double)
     """
     # load map from unpickle
     import pickle
+
     with open('matrix.pickle', 'rb') as handle:
         matrix = pickle.load(handle)
     # multiply the matrix by i
-    matrix = matrix * i
+    matrix = matrix
 
     # convert to torch tensor
     input_tensor = torch.tensor(matrix, dtype=torch.double)
     pool = pytorch_builtin_pooling(input_tensor, KERNEL_SIZE)
 
-    # copy the pooled tensor back to the CPU
-    pool_cpu = pool.cpu()
-
     # create a heat map from the matrix
-    plt.imshow(pool_cpu, cmap='hot', interpolation='nearest', vmin=0, vmax=7500)
+    plt.imshow(pool, cmap='hot', interpolation='nearest', vmin=0, vmax=7500)
     # save the heat map
     plt.savefig('heat_map_' + str(i) + '.png')
     plt.show()
-
---
+++ naive.py
@@ -0,0 +1,86 @@
+#---------------------------------------------------------------------------
+# MAIN.PY
+# Using the GPU to accelerate an electrical power grid visualization
+#
+# Date:                   03/14/2023
+# Authors:                Pragati Dode, Breanna Powell, and William Selke
+#
+# +++++++++++++++++ DETAILS ABOUT SYSTEM ++++++++++++++
+# IDEs:                   Visual Studio Code; PyCharm
+# Host Used:              ____put Will's computer info here
+# Device Used:            ____put Will's computer info here
+# CUDA Version:           _____put Will's computer info here
+# Device Architecture:    Ampere
+#
+# +++++++++++++++++ INSTALLATION INSTRUCTIONS +++++++++++++++++
+# https://numba.readthedocs.io/en/stable/user/installing.html
+#
+# Use the following commands if using Conda:
+# $ conda install cudatoolkit
+# $ conda install tqdm
+#
+# Use the following commands if using pip:
+# $ pip install cuda-python
+#
+# +++++++++++++++++ LIBRARY USED +++++++++++++++++
+# Numba library information: https://numba.readthedocs.io/en/stable/cuda/overview.html
+# Numba library contents: https://numba.readthedocs.io/en/stable/cuda/index.html
+# Note: Numba does not implement: dynamic parallelism and texture memory
+
+from datetime import datetime
+from random import random
+from random import random
+from tqdm import tqdm
+
+from home import home, perlin
+import matplotlib.pyplot as plt
+
+# MAP SIZE x MAP SIZE
+MAP_SIZE = 512
+
+# Perlin is for noise: https://pypi.org/project/perlin/
+homes = perlin(seed=200, map_width=MAP_SIZE, map_height=MAP_SIZE)
+
+# tqdm is a progress bar: https://www.educative.io/answers/what-is-tqdm-library-in-python
+# tqdm = tqdm(total=24 * len(homes))
+
+# -------- UPDATE THE PLOT FOR EVERY HOUR TO CREATE THE SIMULATION -------------
+for hour in range(1):
+    datetime_now = datetime(2020, 1, 1, hour, 0, 0)
+    # plot the home in the graph
+    total_consumption = 0
+    # clear the plot
+    plt.clf()  # clf = clear the figure: https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.clf.html
+
+    # ------------------- INCREMENT THE TOTAL CONSUMPTION --------------------
+    for home in homes:
+        # plot with square as marker
+        # create noise 0.9 - 1.1
+        noise = 0.9 + (1.1 - 0.9) * random()
+        # set the color based on the current consumption
+        cur = home.get_consumption(datetime_now, noise)
+
+        # ---- TAKE THE LARGE DATASET, COMBINE MULTIPLE HOUSES, NAIVE VERSION ----
+        # TO DO: CREATE A LARGE NUMBER OF HOUSES
+        # TO DO: IMPLEMENT A NAIVE STENCIL METHOD THAT COMBINES SEVERAL HOUSES SO THAT
+        #        A LARGE NUMBER OF HOUSES IS COMBINED AND FITS WITHIN A SMALL HEATMAP.
+
+        total_consumption += cur
+        # set closer to red if the consumption is higher green if low
+        color = (1 - min(cur / 10, 1), 1 - min(cur / 10, 1), 1)
+        plt.plot(home.x_coord, home.y_coord, marker='s', color=color)
+        # set plot to Map Size
+
+        # tqdm.update(1)
+
+    plt.axis([0, MAP_SIZE, 0, MAP_SIZE])
+
+    # add a small total_consumption text to the plot
+    plt.text(10, MAP_SIZE - 10, 'Total Consumption: ' + str(total_consumption))
+    # set title as time
+    plt.title(datetime_now)
+    # save plot to file
+    plt.show()
+    # plt.savefig(str(hour) + '.png')
+
+# tqdm.close()
